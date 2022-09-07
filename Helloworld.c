3297a59400fa81064e7ce8e7435f73112c6435ad
Pris042 <priscilia.babalola09@gmail.com>
2022-09-07 21:39:16 +0800

    Committing basic Helloworld program

---
+++ Helloworld.c
@@ -0,0 +1,19 @@
+/*
+    Basic Code for printing hello world to the screen 
+
+    Author: Priscilia Babalola
+*/
+
+/* include library */
+#include <stdio.h>
+
+/* main function */ 
+int main()
+{   
+    /* print the message and then a newline */
+    printf("Hello world!\n");
+
+
+    /* return with no error */
+    return(0);
+}
